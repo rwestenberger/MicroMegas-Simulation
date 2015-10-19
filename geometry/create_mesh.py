8c600c425feb6879d705bfc7bba8e68f3767fc65
Robert <rwestenb@students.uni-mainz.de>
2015-10-19 14:08:33 +0200

    Adapt the GUI-adjustable parameters

--- geometry/create_mesh.py
+++ geometry/create_mesh.py
@@ -15,27 +15,33 @@ class MeshGenerator(QtGui.QWidget):
 		
 	def initUI(self):
 		# Parameters
-		self.label_wire_count = QtGui.QLabel("Wire count ",self)
-		self.sb_wire_count = QtGui.QSpinBox(self)
-		self.sb_wire_count.setValue(4)
-		self.sb_wire_count.setMaximum(1000)
-		self.sb_wire_count.setMinimum(1)
-		self.sb_wire_count.setEnabled(False)
-
-		self.cb_create_unit = QtGui.QCheckBox("Create Unit Cell", self)
-		self.cb_create_unit.setCheckState(QtCore.Qt.Checked)
-
-		self.label_mesh_lattice_const = QtGui.QLabel(u"Mesh lattice constant (µm) ",self)
+		self.label_mesh_lattice_const = QtGui.QLabel(u"Mesh lattice constant (µm) ", self)
 		self.sb_mesh_lattice_const = QtGui.QDoubleSpinBox(self)
-		self.sb_mesh_lattice_const.setValue(62.5)
 		self.sb_mesh_lattice_const.setMaximum(1000.)
 		self.sb_mesh_lattice_const.setMinimum(.01)
+		self.sb_mesh_lattice_const.setSingleStep(.1)
+		self.sb_mesh_lattice_const.setValue(62.5)
 
-		self.label_wire_diameter = QtGui.QLabel(u"Wire diameter (µm) ",self)
+		self.label_wire_diameter = QtGui.QLabel(u"Wire diameter (µm) ", self)
 		self.sb_wire_diameter = QtGui.QDoubleSpinBox(self)
-		self.sb_wire_diameter.setValue(28.)
 		self.sb_wire_diameter.setMaximum(500.)
 		self.sb_wire_diameter.setMinimum(.01)
+		self.sb_wire_diameter.setSingleStep(.1)
+		self.sb_wire_diameter.setValue(28.)
+
+		self.label_space_above_mesh = QtGui.QLabel(u"Space above mesh (µm) ", self)
+		self.sb_space_above_mesh = QtGui.QDoubleSpinBox(self)
+		self.sb_space_above_mesh.setMaximum(10000.)
+		self.sb_space_above_mesh.setMinimum(0.)
+		self.sb_space_above_mesh.setSingleStep(1.)
+		self.sb_space_above_mesh.setValue(300.)
+
+		self.label_space_below_mesh = QtGui.QLabel(u"Space below mesh (µm) ", self)
+		self.sb_space_below_mesh = QtGui.QDoubleSpinBox(self)
+		self.sb_space_below_mesh.setMaximum(1000.)
+		self.sb_space_below_mesh.setMinimum(0.)
+		self.sb_space_below_mesh.setSingleStep(1.)
+		self.sb_space_below_mesh.setValue(150.)
 
 		# Ok and cancel buttons
 		self.button_create = QtGui.QPushButton("Create Mesh", self)
@@ -43,33 +49,28 @@ class MeshGenerator(QtGui.QWidget):
 
 		# Layout
 		layout = QtGui.QGridLayout()
-		layout.addWidget(self.label_wire_count, 1, 0)
-		layout.addWidget(self.sb_wire_count, 1, 1)
-		layout.addWidget(self.cb_create_unit, 2, 0)
-		layout.addWidget(self.label_mesh_lattice_const, 3, 0)
-		layout.addWidget(self.sb_mesh_lattice_const, 3, 1)
-		layout.addWidget(self.label_wire_diameter, 4, 0)
-		layout.addWidget(self.sb_wire_diameter, 4, 1)
-		layout.addWidget(self.button_create, 5, 0)
-		layout.addWidget(self.button_exit, 5, 1)
+		layout.addWidget(self.label_mesh_lattice_const, 0, 0)
+		layout.addWidget(self.sb_mesh_lattice_const, 0, 1)
+		layout.addWidget(self.label_wire_diameter, 1, 0)
+		layout.addWidget(self.sb_wire_diameter, 1, 1)
+		layout.addWidget(self.label_space_above_mesh, 2, 0)
+		layout.addWidget(self.sb_space_above_mesh, 2, 1)
+		layout.addWidget(self.label_space_below_mesh, 3, 0)
+		layout.addWidget(self.sb_space_below_mesh, 3, 1)
+		layout.addWidget(self.button_create, 4, 0)
+		layout.addWidget(self.button_exit, 4, 1)
 		self.setLayout(layout)
 		# Connectors
-		QtCore.QObject.connect(self.button_create, QtCore.SIGNAL("pressed()"),self.draw_mesh)
+		QtCore.QObject.connect(self.button_create, QtCore.SIGNAL("pressed()"),self.generate_unit_cell)
 		QtCore.QObject.connect(self.button_exit, QtCore.SIGNAL("pressed()"),self.Close)
-		self.cb_create_unit.stateChanged.connect(self.create_unit_state)
 
-	def create_unit_state(self, state):
-		if state == QtCore.Qt.Checked:
-			self.sb_wire_count.setEnabled(False)
-		else:
-			self.sb_wire_count.setEnabled(True)
-
-	def draw_mesh(self):
-		'''Generates a complete mesh or unit cell from single mesh wires.'''
+	def generate_unit_cell(self):
+		'''Generates a complete unit cell from single mesh wires.'''
 		try:
-			wire_count = 4 if self.cb_create_unit.isChecked() else int(self.sb_wire_count.text())
 			mesh_lattice_const = float(self.sb_mesh_lattice_const.text())*1e-3
 			wire_diameter = float(self.sb_wire_diameter.text())*1e-3
+			space_above_mesh = float(self.sb_space_above_mesh.text())*1e-3
+			space_below_mesh = float(self.sb_space_below_mesh.text())*1e-3
 		except:
 			FreeCAD.Console.PrintError("Error in evaluating the parameters")
 
@@ -78,9 +79,8 @@ class MeshGenerator(QtGui.QWidget):
 		wires = {}
 		for direction in ["x", "y"]:
 			wires[direction] = []
-			for wire_num in range(wire_count+1):
-				wire = self.draw_wire(wire_diameter, wire_count, mesh_lattice_const, safety_distance=safety_distance)
-
+			for wire_num in range(5): # draw 4 wires in each direction
+				wire = self.generate_wire(wire_diameter, mesh_lattice_const, safety_distance=safety_distance)
 				# position wire
 				if direction == "x":
 					wire.Placement.Base += FreeCAD.Vector(0, wire_num*mesh_lattice_const, 0)
@@ -100,29 +100,29 @@ class MeshGenerator(QtGui.QWidget):
 
 		mesh = FreeCAD.activeDocument().addObject("Part::MultiFuse","Mesh")
 		mesh.Shapes = wires["x"] + wires["y"]
-		mesh.Placement.Base = FreeCAD.Vector(-wire_count/2.*mesh_lattice_const, -wire_count/2.*mesh_lattice_const, 0) # translate mesh to origin
+		mesh.Placement.Base = FreeCAD.Vector(-2.*mesh_lattice_const, -2.*mesh_lattice_const, 0) # translate mesh to origin
 
-		if self.cb_create_unit.isChecked():
-			unit_cell_bounding_box = FreeCAD.activeDocument().addObject("Part::Box","Unit Cell Bounding Box")
-			unit_cell_bounding_box.Length = mesh_lattice_const*2.
-			unit_cell_bounding_box.Width = mesh_lattice_const*2.
-			unit_cell_bounding_box.Height = mesh_lattice_const*2.
-			unit_cell_bounding_box.Placement.Base = FreeCAD.Vector(-mesh_lattice_const, -mesh_lattice_const, -mesh_lattice_const)
+		gas_box = FreeCAD.activeDocument().addObject("Part::Box","Gas")
+		gas_box.Length = mesh_lattice_const*2.
+		gas_box.Width = mesh_lattice_const*2.
+		gas_box.Height = space_above_mesh + space_below_mesh + wire_diameter*2.
+		gas_box.Placement.Base = FreeCAD.Vector(-mesh_lattice_const, -mesh_lattice_const, -space_below_mesh-wire_diameter)
 
-			unit_cell = FreeCAD.activeDocument().addObject("Part::MultiCommon", "Unit Cell")
-			unit_cell.Shapes = [unit_cell_bounding_box, mesh]
+		unit_cell = FreeCAD.activeDocument().addObject("Part::Cut", "Unit Cell")
+		unit_cell.Base = gas_box
+		unit_cell.Tool = mesh
 		
 		mesh.touch() # to force recomputation
 		FreeCAD.ActiveDocument.recompute()
 
 		
-	def draw_wire(self, wire_diameter, wire_count, mesh_lattice_const, safety_distance=0.1e-3):
+	def generate_wire(self, wire_diameter, mesh_lattice_const, safety_distance=0.1e-3):
 		'''Generates a single mesh wire in x direction.'''
 		vectors = []
-		num_steps = wire_count*5
+		num_steps = 20
 		for i in range(int(num_steps)+1):
-			t = float(i)/float(num_steps) * 2*pi * wire_count/2. + pi/2. # to start at max/min
-			vector_x = wire_count * mesh_lattice_const * float(i)/float(num_steps)
+			t = float(i)/float(num_steps) * 2*pi * 2. + pi/2. # to start at max/min
+			vector_x = 4. * mesh_lattice_const * float(i)/float(num_steps)
 			vector_z = sin(t)*(wire_diameter/2.+safety_distance)
 			vectors.append(FreeCAD.Vector(vector_x, 0, vector_z))
 
