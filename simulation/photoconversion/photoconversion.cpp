2c559ac83d0929199c4b0b7e292b1b6bc1e04a04
Robert <rwestenb@students.uni-mainz.de>
2015-11-03 18:03:15 +0100

    Change from .cc to .cpp and seg fault fix

--- simulation/photoconversion/photoconversion.cpp
+++ simulation/photoconversion/photoconversion.cpp
@@ -18,9 +18,7 @@
 int main(int argc,char** argv) {
 	// Detect interactive mode (if no arguments) and define UI session
 	G4UIExecutive* ui = 0;
-	if (argc == 1) {
-		ui = new G4UIExecutive(argc, argv);
-	}
+	if (argc == 1) ui = new G4UIExecutive(argc, argv);
 
 	// Choose the Random engine
 	G4Random::setTheEngine(new CLHEP::RanecuEngine);
@@ -58,16 +56,17 @@ int main(int argc,char** argv) {
 
 	// Process macro or start UI session
 	//
-	if (!ui) {
+	if (ui) {
+		// interactive mode
+		UImanager->ApplyCommand("/control/macroPath .."); // set correct macro path
+		//UImanager->ApplyCommand("/control/execute vis.mac"); // can cause seg fault
+		ui->SessionStart();
+		delete ui;
+	} else {
 		// batch mode
 		G4String command = "/control/execute ";
 		G4String fileName = argv[1];
 		UImanager->ApplyCommand(command+fileName);
-	} else {
-		// interactive mode
-		UImanager->ApplyCommand("/control/execute init_vis.mac");
-		ui->SessionStart();
-		delete ui;
 	}
 
 	// Job termination
