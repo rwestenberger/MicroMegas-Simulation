4007cf49c0d44d6b30f4e2c8b5eacfe9b78853bb
Robert <rwestenb@students.uni-mainz.de>
2015-11-10 18:25:04 +0100

    Add circular electron angle plot

--- simulation/photoconversion/plot.C
+++ simulation/photoconversion/plot.C
@@ -3,24 +3,31 @@ TFile* inFile = new TFile("build/photoconversion.root");
 
 gStyle->SetOptStat(11);
 
-TH2F* directionHist = (TH2F*)inFile->Get("directionHist");
+TTree* conversionTree = (TTree*)inFile->Get("conversionTree");
 
 TCanvas* c1 = new TCanvas("c1", "Photoconversion", 1200, 600);
 c1->Divide(2,1);
 
 c1->cd(1);
+//TH1F* directionHist = new TH1F("Direction", "", 60, 0., TMath::Pi());
+TH2F* directionHist = new TH2F("Direction", "", 60, -TMath::Pi(), TMath::Pi(), 20, 0., TMath::Pi()/2.);
+conversionTree->Draw("theta:phi>>Direction", "", "");
 gPad->SetTheta(90);
 gPad->SetPhi(180);
-directionHist->Rebin2D(2,4);
+//directionHist->Rebin2D(1,2);
 directionHist->Draw("pollego2z");
-TGraphPolargram* gp = new TGraphPolargram("g", directionHist->GetYaxis()->GetXmin(), directionHist->GetYaxis()->GetXmax(), 0, 2*TMath::Pi());
-gp->SetNdivRadial(4);
+TGraphPolargram* gp = new TGraphPolargram("g", directionHist->GetYaxis()->GetXmin(), directionHist->GetYaxis()->GetXmax(), 0, TMath::Pi());
+gp->SetNdivRadial(10);
 gp->SetNdivPolar(4);
+gp->SetTitle("Direction");
 gp->Draw();
 gPad->Update();
+// directionHist->Draw();
 
 c1->cd(2);
-energyHist->Rebin(4);
+conversionTree->Draw("Ekin>>Energy", "", "");
+TH1F* energyHist = (TH1F*)gDirectory->Get("Energy");
+energyHist->SetTitle(";E_{kin} [keV]");
 energyHist->Draw();
 
 c1->SaveAs("plot.png");
