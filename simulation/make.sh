82769991e368d0345cb3cc5fe0796ca7ee5cadc5
Robert <rwestenb@students.uni-mainz.de>
2015-11-30 11:43:18 +0100

    Add make.sh to make all simulations

---
+++ simulation/make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+project_path="$(pwd)"
+export PYTHONPATH="${PYTHONPATH}:$project_path"
+
+echo "Building photoconversion..."
+cd "$project_path/photoconversion"
+mkdir -p "build" && cd "build"
+cmake -DCMAKE_INSTALL_PREFIX=.. ..
+make && make install
+
+echo "Building drift..."
+cd "$project_path/drift"
+make
+
+echo "Building avalanche..."
+cd "$project_path/avalanche"
+make
+
+echo "Done..."
