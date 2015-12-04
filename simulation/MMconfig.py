09f8bb2595aa3040cc8ba819427406d9d7002a23
Robert <rwestenb@students.uni-mainz.de>
2015-12-04 10:51:49 +0100

    Create output directory if needed

--- simulation/MMconfig.py
+++ simulation/MMconfig.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#import cog
+import cog
 from configparser import ConfigParser, ExtendedInterpolation
 import os
 
 conf = ConfigParser(interpolation=ExtendedInterpolation())
 conf.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), "simulation.conf"))
+
+if not os.path.exists(conf["general"]["output_path"]):
+	os.makedirs(conf["general"]["output_path"])
+	print("Created output directory: {}".format(conf["general"]["output_path"]))
 
 '''In the source file you now just need to do:
 	/*[[[cog
