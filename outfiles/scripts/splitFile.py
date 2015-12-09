4a809cd4ed71316ea6a38336271f83a62cd3a33c
rwestenberger <rwestenb@students.uni-mainz.de>
2015-12-09 11:49:14 +0100

    Output leading zeros, debug input parameter processing

--- outfiles/scripts/splitFile.py
+++ outfiles/scripts/splitFile.py
@@ -9,21 +9,26 @@ def get_tree_list(input_file):
 
 def check_for_tree(input_file, input_tree_name):
 	tree_list = get_tree_list(input_file)
+
 	if len(tree_list) == 0:
 		print('No tree in %s found!'%(input_file_name))
 		sys.exit(1)
 	
-	if len(tree_list) > 1 and not input_tree_name:
-		print('Multiple trees found: %s. Provide -t/--tree!'%(', '.join(tree_list)))
-		sys.exit(1)
+	if input_tree_name:
+		if input_tree_name not in tree_list:
+			print('Could not find tree %s in %s'%(input_tree_name, input_file_name))
+			sys.exit(1)
+		else:
+			return input_tree_name
+	else:
+		if len(tree_list) == 1:
+			print('Choosing tree %s to split.'%(tree_list[0]))
+			return tree_list[0]
 
-	if len(tree_list) == 1 and not input_tree_name:
-		input_tree_name = tree_list[0]
-		print('Choosing tree %s to split.'%(input_tree_name))
-
-	if input_tree_name not in tree_list:
-		print('Could not find tree %s in %s'%(input_file_name, input_file_name))
-		sys.exit(1)
+		if len(tree_list) > 1:
+			print('Multiple trees found: %s. Provide -t/--tree!'%(', '.join(tree_list)))
+			sys.exit(1)
+	return None
 
 def splitFile(args):
 	from ROOT import TFile, TTree # here, otherwise -h/--help will not work
@@ -34,23 +39,32 @@ def splitFile(args):
 
 	if not args.batch:
 		if args.nevents:
-			print('Splitting tree %s of file %s into parts of %d events each.'%(args.tree, args.inputFileName, args.nevents))
+			print('Splitting file %s into parts of %d events each.'%(args.inputFileName, args.nevents))
 		else:
-			print('Splitting tree %s of file %s into %d parts.'%(args.tree, args.inputFileName, args.njobs))
+			print('Splitting file %s into %d parts.'%(args.inputFileName, args.njobs))
 
 	input_file = TFile(args.inputFileName, 'read')
-	check_for_tree(input_file, args.tree)
+	args.tree = check_for_tree(input_file, args.tree)
+	if not args.tree:
+		print('Error getting the right tree name!')
+		sys.exit(1)
 	input_tree = input_file.Get(args.tree)
 
 	output_file_names = []
 	nevents = input_tree.GetEntries()
 
+	if not args.batch:
+		print('Found %d events in tree %s.'%(nevents, args.tree))
+
 	if args.njobs:
 		args.nevents = int(math.ceil(float(nevents) / args.njobs))
 
-	for file_num, event_nums in enumerate([range(nevents)[i:i+args.nevents] for i in range(0, nevents, args.nevents)]):
+	event_blocks = list(enumerate([range(nevents)[i:i+args.nevents] for i in range(0, nevents, args.nevents)]))
+	last_block_num = event_blocks[-1][0]
+
+	for file_num, event_nums in event_blocks:
 		input_file_base, extension = os.path.splitext(args.inputFileName)
-		output_file_name = '%s_%s%s'%(input_file_base, file_num, extension)
+		output_file_name = '%s_%s%s'%(input_file_base, str(file_num).zfill(len(str(last_block_num))), extension)
 		output_file_names.append(output_file_name)
 		output_file = TFile(output_file_name, 'recreate')
 		output_tree = input_tree.CloneTree(0);
