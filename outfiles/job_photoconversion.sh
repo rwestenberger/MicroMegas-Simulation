d1b70bccbb903f65be32a633ce29b0b0562f9d49
rwestenberger <rwestenb@students.uni-mainz.de>
2015-12-11 10:28:28 +0100

    Add photoconversion job script

---
+++ outfiles/job_photoconversion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+        echo "Usage: $(basename $0) DIRECTORY"
+        exit 1
+fi
+
+WD="$1"
+
+PHOTOCONVERSION_EXEC="/home/rwestenb/simulation/MicroMegas-Simulation/simulation/photoconversion/build/photoconversion"
+
+echo "Using simulation directory: $WD"
+
+# ROOT and Garfield++ setup
+source /cluster/cern/root_v5.34.03_x86_64/bin/thisroot.sh
+
+# needs about 300M ram for 100k photons
+$PHOTOCONVERSION_EXEC > $WD/photoconversion.log
+
+STATUS=$?
+exit $STATUS
+
