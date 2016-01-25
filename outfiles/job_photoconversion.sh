79a20a7c277b730fe2331d48c4932b3d805385ec
rwestenberger <rwestenb@students.uni-mainz.de>
2016-01-25 10:24:15 +0100

    job_photoconversion.sh uses absolute paths

--- outfiles/job_photoconversion.sh
+++ outfiles/job_photoconversion.sh
@@ -10,7 +10,7 @@ if [ ! -e "$1" ]; then
 	mkdir "$1"
 fi
 
-WD="$1"
+WD="$(readlink -f $1)"
 
 PHOTOCONVERSION_EXEC="/home/rwestenb/simulation/MicroMegas-Simulation/simulation/photoconversion/build/photoconversion"
 
@@ -19,8 +19,9 @@ echo "Using simulation directory: $WD"
 # ROOT and Garfield++ setup
 source /cluster/cern/root_v5.34.03_x86_64/bin/thisroot.sh
 
+cd $(dirname "$PHOTOCONVERSION_EXEC")
 # needs about 300M ram for 100k photons
-$PHOTOCONVERSION_EXEC > $WD/photoconversion.log
+./$(basename "$PHOTOCONVERSION_EXEC") > $WD/photoconversion.log
 
 STATUS=$?
 exit $STATUS
