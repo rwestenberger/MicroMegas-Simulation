36c9cfab2d18a3dc998ae84376f35866c7094f45
Robert <rwestenb@students.uni-mainz.de>
2016-04-19 13:36:18 +0200

    Local execution of simulation parts

--- simulation/run.sh
+++ simulation/run.sh
@@ -3,10 +3,13 @@
 project_path="$(pwd)"
 
 echo "Running photoconversion..."
-"$project_path/photoconversion/build/photoconversion"
+cd "$project_path/photoconversion/build/"
+./photoconversion
 
 echo "Running drift..."
-"$project_path/drift/drift"
+cd "$project_path/drift/"
+./drift
 
 echo "Running avalanche..."
-"$project_path/avalanche/avalanche"
+cd "$project_path/avalanche/"
+./avalanche
